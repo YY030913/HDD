ce63f01156b2141ae52de88d9bf84db15d4d199d
Jesse Zhu <jesse@itjesse.cn>
2013-11-25 21:22:46 +0800

    解决首页version和值班人员变量冲突问题

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 include "./config/config.php";
 $sql="SELECT * FROM hdd_update_log ORDER BY time DESC";
-$result=@mysql_query($sql) or die;
-$row=mysql_fetch_array($result);
-echo $row['version'];
+$_result=@mysql_query($sql) or die;
+$_row=mysql_fetch_array($result);
+echo $_row['version'];
 ?>
